f1e1f6aa2fd4b9159ba296ea29b85b7e337336af
Ryan Blair <rblair@gmail.com>
2013-12-16 01:24:03 +0000

    update puppet (3.3.2) but have not tested due to ms ruby-19/r151008 incompatibility

--- build/puppet/build.sh
+++ build/puppet/build.sh
@@ -29,7 +29,7 @@
 . ../../lib/functions.sh
 
 PROG=puppet     # App name
-VER=3.3.0       # App version
+VER=3.3.2       # App version
 VERHUMAN=$VER   # Human-readable version
 #PVER=          # Branch (set in config.sh, override here if needed)
 PKG=${PKGPUBLISHER}/system/management/puppet  # Package name (e.g. library/foo)
